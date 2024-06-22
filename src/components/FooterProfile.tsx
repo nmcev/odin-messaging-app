88be628bd93c38111d5f6e383478b82ca212df21
nmcev <1student.muhammed1@gmail.com>
2024-06-22 22:31:24 +0300

    feat: implement profile footer component

---
+++ src/components/FooterProfile.tsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+import logoutIcon from '../assets/logout.svg'
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+
+interface Data {
+    user: User;
+    joinedAt: string;
+  
+  
+}
+
+  interface User {
+    username: string;
+    _id: string;
+    profilePic: string;
+  
+}
+interface FooterProfileProps {
+    currentUser:  Data
+}
+
+export const FooterProfile: React.FC<FooterProfileProps> = ({currentUser}) => {
+
+    const authContext = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    if (!authContext || !currentUser) {
+        return null
+    }
+
+    const  { logout } = authContext
+  return (
+
+    <footer className="absolute bottom-0 left-0 right-0 bg-[#2b2b2b] text-white py-2 px-4 flex  items-center justify-between gap-5">
+
+          <div className="flex items-center cursor-pointer " onClick={() => navigate(`/user/${currentUser.user.username}`)}>
+              <div className="w-12 h-12">
+                  <img className="rounded-full w-full h-full object-cover" src={currentUser.user.profilePic} alt="user avatar" />
+              </div>
+
+              <div className="ml-4">
+                <h2 className="text-xl font-bold">{currentUser.user.username}</h2>
+              </div>
+          </div>
+            
+            <button className=" cursor-pointer w-8 h-8 bg-transparent" onClick={logout}>
+              <img  src={logoutIcon} alt="logout icon" />
+            </button>
+    </footer>
+)      
+  
+}
