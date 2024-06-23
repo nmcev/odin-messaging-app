73a511e8bfaf5dc9df9a0bdbb0dce3d153ea360e
nmcev <1student.muhammed1@gmail.com>
2024-06-24 02:12:18 +0300

    refactor: Add UserContextProvider to App component for managing user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom"
 import React from "react"
 import { AuthProvider } from "./context/AuthContext"
+import { UserContextProvider } from "./context/UserContext"
+
 const  App: React.FC = () => {
   return (
+    <UserContextProvider>
     <AuthProvider>
     <div className='flex flex-col min-h-screen'>
         <Outlet />
     </div>
     </AuthProvider>
+    </UserContextProvider>
   )
 }
 
