edcd9171dd42f1d0ba26f276e17ec38d4593b09a
nmcev <1student.muhammed1@gmail.com>
2024-06-23 14:56:03 +0300

    feat: enable to navigate users page from the search results

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 interface User {
     _id: string;
@@ -10,13 +11,13 @@ interface SearchResultProps {
     results:  User[];
 }
 export const SearchResult: React.FC<SearchResultProps> = ({results}) => {
-
+const navigate = useNavigate();
 
   return (
     <div className="flex flex-col gap-8 pl-16 mt-6">
     { results && (
       results.map((user) => (
-         <div key={user._id} className="flex items-center cursor-pointer ">
+         <div key={user._id} className="flex items-center cursor-pointer" onClick={() => navigate(`/user/${user.username}`)}>
           <div className="w-12 h-12">
             <img className="rounded-full w-full h-full object-cover" src={user.profilePic} alt="user avatar" />
           </div>
