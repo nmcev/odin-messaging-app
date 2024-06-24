7f1316633b0886437d009c43f542a80a6f1420ba
nmcev <1student.muhammed1@gmail.com>
2024-06-24 03:20:34 +0300

    feat: Add UsersList component for displaying a list of users that you talked with

---
+++ src/components/UsersList.tsx
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+
+interface User {
+    username: string;
+    _id: string;
+    profilePic: string;
+    lastMessage: string; 
+    lastMessageSendAt: string;
+}
+
+interface UsersListProps {
+    users: User[];
+}
+
+export const UsersList: React.FC<UsersListProps> = ({ users }) => {
+    const userContext = useContext(UserContext);
+    const { setChattingWith } = userContext;
+
+    const handleOpenChat = (user: User) => {
+        setChattingWith(user);
+    };
+
+    const formatLastMessage = (timestamp: string): string => {
+        const date = new Date(timestamp);
+        const now = new Date();
+
+        const diff = now.getTime() - date.getTime();
+        const hoursDiff = diff / (1000 * 60 * 60);
+
+        if (hoursDiff < 24) {
+            const hours = date.getHours().toString().padStart(2, '0');
+            const minutes = date.getMinutes().toString().padStart(2, '0');
+            return `${hours}:${minutes}`;
+        } else {
+            const day = date.getDate().toString().padStart(2, '0');
+            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            return `${day}/${month}`;
+        }
+    };
+
+    return (
+        <section className="flex flex-col gap-8 pl-10 mt-4 ">
+            {users && (
+                users.map((data) => (
+                    <div key={data._id} className="flex items-center cursor-pointer hover:bg-[#1919194b] rounded-md p-2 mx-3" onClick={() => handleOpenChat(data)}>
+                        <div className="w-12 h-12 flex-shrink-0">
+                            <img className="rounded-full w-full h-full object-cover" src={data.profilePic} alt="user avatar" />
+                        </div>
+                        <div className="ml-4 flex justify-between  w-full items-center ">
+                            <div className="flex justify-center flex-col  ">
+                            <h2 className="text-xl font-bold">{data.username}</h2>
+                            <p className="text-gray-500">{
+                                data.lastMessage.startsWith('https://odin-blog-bucket.s3.eu-north-1') ? (
+                                    'Image'
+                                ) : data.lastMessage.length > 30 ? (
+                                    `${data.lastMessage.substring(0, 30)}...`
+                                ) : data.lastMessage
+                                }</p>
+                            </div>
+                            <div className=''>
+                                <p className="text-sm text-gray-600">{formatLastMessage(data.lastMessageSendAt)}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                ))
+            )}
+        </section>
+    );
+};
