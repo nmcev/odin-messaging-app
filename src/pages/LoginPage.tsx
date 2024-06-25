983e9dfca2544ec450644f262e9e06264aa695d3
nmcev <1student.muhammed1@gmail.com>
2024-06-25 17:37:11 +0300

    refactor: Add autoComplete attribute to username input in LoginPage component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -63,6 +63,7 @@ import { AuthContext } from '../context/AuthContext';
                     value ={username}
                     onChange={(e) => setUsername(e.target.value.trim())}
                     required
+                    autoComplete='username'
                     className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-300 focus:border-orange-300 sm:text-sm"
                   />
                   {
