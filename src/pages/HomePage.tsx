c29154804e0ebac165da4661aba828ef403774fa
muha <1student.muhammed1@gmail.com>
2026-09-18 00:08:12 +0300

    fix remove ui tabs from use effect dependency array

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -127,7 +127,7 @@ export const HomePage: React.FC = () => {
           newSocket.disconnect();
         };
       });
-    }, [authContext.currentUser.user._id, setGlobalMessages, setMessages, isUsersOpen, isChatsOpen]);
+    }, [authContext.currentUser.user._id, setGlobalMessages, setMessages]);
     
     
 // fetch messages
